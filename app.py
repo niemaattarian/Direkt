5bf2662f287c80110c68e58de0f3bed28f804488
David Neilan <neilan.david@gmail.com>
2020-04-06 22:24:25 +0100

    Implemented drill-down searching

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 import urllib.parse
+from functools import reduce
 
-from flask import Flask, render_template, redirect
-from Menu import MENU_ITEMS
+from flask import Flask, render_template, redirect, request
+from Menu import MENU_ITEMS, MenuItem
 
 app = Flask(__name__)
 
 
-
-
-
 @app.route('/')
 def home():
-    return render_template('index.html', links=MENU_ITEMS)
+    category = request.args.get('category', None)
+
+    if category is not None:
+        keys = [p.strip() for p in category.split('/')]
+
+        try:
+            path = list(reduce(lambda o, k: dict.__getitem__(o, k), keys, MENU_ITEMS))
+            items = list(filter(lambda i: MENU_ITEMS.get_item_by_title(i) is not None, path))
+
+            if len(items) > 0:
+                return render_template('index.html', products=items)
+
+            return render_template('index.html', links=path, path='/'.join(keys) + '/')
+        except KeyError:
+            return redirect('/')
+
+    return render_template('index.html', links=MENU_ITEMS, path='')
 
 
 @app.route('/products/<product_name>')
@@ -27,3 +41,5 @@ def product(product_name):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
+
